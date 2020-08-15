05b15f64b17113a00a6c20ff74695d1b3750e175
Rob Keyer <rob.keyer@eltoro.com>
2020-08-15 15:51:18 -0400

    Updates

---
+++ precache-manifest.c1ee343ea4e34b6d4119417d99a766e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f5e53bcbd474ad261dccf992e32a796",
+    "url": "/HorseBetCalculator/index.html"
+  },
+  {
+    "revision": "87588bf867c32ff3fa59",
+    "url": "/HorseBetCalculator/static/css/main.9e0185c6.chunk.css"
+  },
+  {
+    "revision": "81bc6970994367f29c98",
+    "url": "/HorseBetCalculator/static/js/2.d011ec2f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/HorseBetCalculator/static/js/2.d011ec2f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87588bf867c32ff3fa59",
+    "url": "/HorseBetCalculator/static/js/main.ac5f2911.chunk.js"
+  },
+  {
+    "revision": "819e507fc03aab05fd62",
+    "url": "/HorseBetCalculator/static/js/runtime-main.b75726dd.js"
+  }
+]);
