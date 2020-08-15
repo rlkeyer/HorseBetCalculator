ac293bd60a999abe8707b4bc2c98329b2970e3c0
Rob Keyer <rob.keyer@eltoro.com>
2020-08-15 16:13:34 -0400

    added route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './App.css';
+import { Switch, Route } from 'react-router-dom'
 import { Calculator } from './components/Calculator'
 
 function App() {
   return (
-    <div className="App">
-      <Calculator />
-    </div>
+    <Switch>
+      <div className="App">
+        <Route exact path="/" component={Calculator} />
+      </div>
+    </Switch>
   );
 }
 
